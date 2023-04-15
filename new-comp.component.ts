837ac9f4b238861cf0138cc76a95486d5739fda5
sumitkk19 <65333951+sumitkk19@users.noreply.github.com>
2023-04-15 23:40:32 +0530

    Add files via upload

---
+++ new-comp.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-comp',
+  templateUrl: './new-comp.component.html',
+  styleUrls: ['./new-comp.component.css']
+})
+export class NewCompComponent implements OnInit {
+{
+
+  public Name = "Marvellous"
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  fun()
+  {
+    return "Learn " + this.Name;
+  }
+}
+}
